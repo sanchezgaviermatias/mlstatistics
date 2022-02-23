92a13677306e12b4f1b17b958fa479bcd9910466
Matias Sanchez <sanchezgaviermatias@gmail.com>
2022-02-23 19:53:55 -0300

    Agregue funciones de hash, init has, print table, hash

--- mlstatistics/Source.cpp
+++ mlstatistics/Source.cpp
@@ -1,26 +1,28 @@
-
 #include "vector_utils.h"
-
-
-
-
-
+#include "MapHash.h"
 
 
 
 int main() {
-	int tamaño = 10;
-	vector vector_prueba =	create_zero_vector(tamaño);
-	log_vector(&vector_prueba);
+	//	int tamaño = 10;
+	//	vector vector_prueba =	create_zero_vector(tamaño);
+	//	log_vector(&vector_prueba);
 
-	float media = mean(vector_prueba);
-	float varianza = var(vector_prueba);
-	float stand = stad(vector_prueba);
+	//	float media = mean(vector_prueba);
+	//	float varianza = var(vector_prueba);
+	//	float stand = stad(vector_prueba);
 
 
-	printf("media %f\n", media);
-	printf("varianza %f\n", varianza);
-	printf("std %f\n", stand);
+	//	printf("media %f\n", media);
+	//	printf("varianza %f\n", varianza);
+	//	printf("std %f\n", stand);
+
+	printf("InversaNormal => %u\n", hash("InversaNormal"));
+	printf("Normal => %u\n", hash("Normal"));
+	printf("Beta => %u\n", hash("Beta"));
+
+
+	
 
 
 
